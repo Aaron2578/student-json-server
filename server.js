0442eebae1f8b2db359b495408047345d2dfd19d
Aaron <115331438+Aaron2578@users.noreply.github.com>
2025-11-10 17:30:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(jsonServer.bodyParser);
-server.use(router);
-
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000')
+})
